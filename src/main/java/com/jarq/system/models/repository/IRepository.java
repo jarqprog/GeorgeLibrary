8fdf5647452b890e83c7cf025f0a91cb452db1e2
jarqprog <jarqprog@gmail.com>
2018-04-29 21:35:57 +0200

    add repository package

--- src/main/java/com/jarq/system/models/repository/IRepository.java
+++ src/main/java/com/jarq/system/models/repository/IRepository.java
@@ -1,12 +1,30 @@
-package com.jarq.models.repository;
+package com.jarq.system.models.repository;
 
-import com.jarq.models.text.Text;
+import com.jarq.system.models.text.Text;
 
 import java.util.List;
 
 public interface IRepository {
 
     void setTexts(List<Text> texts);
+
     List<Text> getTexts();
+
     int getId();
+
+    void setId(int id);
+
+    String getName();
+
+    void setName(String name);
+
+    String getCreationDate();
+
+    void setCreationDate(String creationDate);
+
+    String getLastModificationDate();
+
+    void setLastModificationDate(String lastModificationDate);
+
+    int getOwnerId();
 }
