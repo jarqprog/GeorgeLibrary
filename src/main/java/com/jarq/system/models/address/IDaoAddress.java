28bade5e95c764441c830d9317a8c64f00bca6f3
jarqprog <jarqprog@gmail.com>
2018-04-29 20:26:02 +0200

    add interface IDaoAddress

--- src/main/java/com/jarq/system/models/address/IDaoAddress.java
+++ src/main/java/com/jarq/system/models/address/IDaoAddress.java
@@ -1,13 +1,23 @@
 package com.jarq.system.models.address;
 
+import com.jarq.system.dao.Dao;
+import com.jarq.system.exceptions.DaoFailure;
+
 import java.sql.SQLException;
 
-public interface IDaoAddress {
+public interface IDaoAddress extends Dao {
 
     IAddress createNullAddress();
-    IAddress createAddress() throws SQLException, ;
-    IAddress importAddress();
-    boolean updateAddress(IAddress address);
 
+    IAddress createAddress(String postalCode, String city, String street) throws SQLException, DaoFailure;
 
+    IAddress importAddress(int addressId) throws SQLException, DaoFailure;
+
+    boolean updateAddress(IAddress address) throws SQLException, DaoFailure;
+
+    boolean exportAddress(IAddress address) throws SQLException, DaoFailure;
+
+    boolean removeAddress(IAddress address) throws SQLException, DaoFailure;
+
+    boolean removeAddress(int addressId) throws SQLException, DaoFailure;
 }
