58f02d48a21489534bb4ed42dd2fc2b73f86f67f
jarqprog <jarqprog@gmail.com>
2018-04-29 21:47:18 +0200

    add second constructor to DaoFailure exception

--- src/main/java/com/jarq/system/exceptions/DaoFailure.java
+++ src/main/java/com/jarq/system/exceptions/DaoFailure.java
@@ -2,14 +2,15 @@ package com.jarq.system.exceptions;
 
 public class DaoFailure extends Exception {
 
+    private final String exceptionInfo = "Problem occurred in DAO action. ";
     private final String message;
 
     public DaoFailure(String message) {
-        this.message = message;
+        this.message = exceptionInfo + message;
     }
 
     public DaoFailure() {
-        this.message = "Problem occurred in DAO action";
+        this.message = exceptionInfo;
     }
 
     public String getMessage() {
