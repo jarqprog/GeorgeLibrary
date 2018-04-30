f65d79bb3ce5c7004f4df4c831f52513babd90a6
jarqprog <jarqprog@gmail.com>
2018-04-30 12:15:17 +0200

    refactor id DaoFailure

--- src/main/java/com/jarq/system/exceptions/DaoFailure.java
+++ src/main/java/com/jarq/system/exceptions/DaoFailure.java
@@ -2,19 +2,17 @@ package com.jarq.system.exceptions;
 
 public class DaoFailure extends Exception {
 
-    private final String exceptionInfo = "Problem occurred in DAO action. ";
     private final String message;
 
     public DaoFailure(String message) {
-        this.message = exceptionInfo + message;
+        this.message = message;
     }
 
     public DaoFailure() {
-        this.message = exceptionInfo;
+        this.message = "Problem occurred in DAO action.";
     }
 
     public String getMessage() {
         return message;
     }
-
 }
