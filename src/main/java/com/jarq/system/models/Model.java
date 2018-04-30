d2c772c3730b7a1d1933d84276f36cf9010f7cd3
jarqprog <jarqprog@gmail.com>
2018-04-30 10:18:20 +0200

    add abstract Model

---
+++ src/main/java/com/jarq/system/models/Model.java
@@ -0,0 +1,15 @@
+package com.jarq.system.models;
+
+public abstract class Model {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
