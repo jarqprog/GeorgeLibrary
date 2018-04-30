0e97909e136181b73a2c74b3632d63fe14b8712f
jarqprog <jarqprog@gmail.com>
2018-04-30 13:42:31 +0200

    add proper setup database file

--- src/main/resources/setup_script.sql
+++ src/main/resources/setup_script.sql
@@ -1,23 +1,4 @@
-BEGIN TRANSACTION;
-DROP TABLE IF EXISTS `texts`;
-CREATE TABLE IF NOT EXISTS `texts` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`title`	TEXT NOT NULL,
-	`creationDate`	TEXT,
-	`lastModificationDate`	TEXT,
-	`repositoryId`	INTEGER NOT NULL,
-	`content`	TEXT,
-	FOREIGN KEY(`repositoryId`) REFERENCES `repositories`(`id`)
-);
-DROP TABLE IF EXISTS `repositories`;
-CREATE TABLE IF NOT EXISTS `repositories` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`name`	TEXT NOT NULL,
-	`creationDate`	TEXT NOT NULL,
-	`lastModificationDate`	TEXT,
-	`ownerId`	INTEGER NOT NULL,
-	FOREIGN KEY(`ownerId`) REFERENCES `users`(`id`)
-);
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -25,18 +6,37 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`surname`	TEXT NOT NULL,
 	`email`	TEXT NOT NULL UNIQUE,
 	`password`	TEXT NOT NULL,
-	`addressId`	INTEGER NOT NULL,
-	FOREIGN KEY(`addressId`) REFERENCES `addresses`(`id`)
+	`address_id`	INTEGER NOT NULL,
+	FOREIGN KEY(`address_id`) REFERENCES `addresses`(`id`)
+);
+DROP TABLE IF EXISTS `texts`;
+CREATE TABLE IF NOT EXISTS `texts` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`title`	TEXT NOT NULL,
+	`creation_date`	TEXT,
+	`last_modification_date`	TEXT,
+	`repository_id`	INTEGER NOT NULL,
+	`content`	TEXT,
+	FOREIGN KEY(`repository_id`) REFERENCES `repositories`(`id`)
+);
+DROP TABLE IF EXISTS `repositories`;
+CREATE TABLE IF NOT EXISTS `repositories` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL,
+	`creation_date`	TEXT NOT NULL,
+	`last_modification_date`	TEXT,
+	`owner_id`	INTEGER NOT NULL,
+	FOREIGN KEY(`owner_id`) REFERENCES `users`(`id`)
 );
 DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE IF NOT EXISTS `addresses` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`postalCode`	TEXT NOT NULL,
+	`postal_code`	TEXT NOT NULL,
 	`city`	TEXT NOT NULL,
 	`street`	TEXT NOT NULL,
-	`houseNo`	TEXT NOT NULL,
-	`apartmentNo`	TEXT
+	`house_no`	TEXT NOT NULL,
+	`apartment_no`	TEXT
 );
-INSERT INTO `addresses` (id,postalCode,city,street,houseNo,apartmentNo) VALUES (1,'30-830','Kraków','Nad Potokiem','109','207'),
- (2,'42-125','Biała','Studzienna','1722',NULL);
-COMMIT;
+INSERT INTO `addresses` (id,postal_code,city,street,house_no,apartment_no) VALUES (1,'30-830','Kraków','Nad Potokiem','109','207'),
+ (2,'42-125','Biała','Studzienna','1722','');
+
