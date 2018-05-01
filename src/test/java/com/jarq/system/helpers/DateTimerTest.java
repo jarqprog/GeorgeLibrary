d568e4b7cf59a2389bbe1f37784108168fb36c87
jarqprog <jarqprog@gmail.com>
2018-05-01 22:35:32 +0200

    add tests for dao factory

---
+++ src/test/java/com/jarq/system/helpers/DateTimerTest.java
@@ -0,0 +1,15 @@
+package com.jarq.system.helpers;
+
+import com.jarq.AbstractTest;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateTimerTest extends AbstractTest {
+
+    
+
+    @Test
+    public void getCurrentDateTime() {
+    }
+}
