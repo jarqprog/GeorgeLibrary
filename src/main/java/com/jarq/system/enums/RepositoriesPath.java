4d95f1666ecf9995a7b7e6549f58f81b001e675d
jarqprog <jarqprog@gmail.com>
2018-05-02 00:05:25 +0200

    add paths to enum

--- src/main/java/com/jarq/system/enums/RepositoriesPath.java
+++ src/main/java/com/jarq/system/enums/RepositoriesPath.java
@@ -2,7 +2,9 @@ package com.jarq.system.enums;
 
 public enum RepositoriesPath {
 
-    FILES_REPOSITORY("src/main/resources/repositories/");
+    FILES_REPOSITORY("src/main/resources/repositories/"),
+    TEST_FILES_REPOSITORY("src/main/resources/testRepositories/"),
+    TEST_FILE("test");
 
     private String path;
 
