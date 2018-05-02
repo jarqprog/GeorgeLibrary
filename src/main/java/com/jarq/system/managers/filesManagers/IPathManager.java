c7d071640708a20452e4eeb5aefa18dd21b78606
jarqprog <jarqprog@gmail.com>
2018-05-02 20:10:22 +0200

    add interface to PathManager

---
+++ src/main/java/com/jarq/system/managers/filesManagers/IPathManager.java
@@ -0,0 +1,13 @@
+package com.jarq.system.managers.filesManagers;
+
+import java.io.IOException;
+
+public interface IPathManager {
+
+    // return true if path exists
+    boolean inspect(String fullFilepath);
+
+    boolean create(String fullFilepath) throws IOException;
+
+    boolean demolish(String fullFilepath) throws IOException;
+}
