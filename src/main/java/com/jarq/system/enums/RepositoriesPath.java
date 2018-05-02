4fec73a0f147f73ef453903a2ac73630d701f2b4
jarqprog <jarqprog@gmail.com>
2018-05-02 20:11:10 +0200

    add to enum paths to test files

--- src/main/java/com/jarq/system/enums/RepositoriesPath.java
+++ src/main/java/com/jarq/system/enums/RepositoriesPath.java
@@ -6,8 +6,12 @@ public enum RepositoriesPath {
 
     FILES_REPOSITORY("src/main/resources/repositories/"),
     TEST_FILES_REPOSITORY("src/main/resources/testRepositories/"),
-    READER_TEST_FILE("readerTextFile.md"),
-    WRITER_TEST_FILE("writerTextFile.md");
+    TEST_FILE("testFile.md"),
+    READER_TEST_FILE("src/main/resources/testRepositories/readerTextFile.md"),
+    WRITER_TEST_FILE("src/main/resources/testRepositories/writerTextFile.md"),
+    MANAGER_PATH_INSPECT_TEST("src/main/resources/testRepositories/pathManager/inspect/1.md"),
+    MANAGER_PATH_CREATION_TEST("src/main/resources/testRepositories/pathManager/create/1.md"),
+    MANAGER_PATH_DEMOLISH_TEST("src/main/resources/testRepositories/pathManager/demolish/1.md");
 
     private String path;
 
