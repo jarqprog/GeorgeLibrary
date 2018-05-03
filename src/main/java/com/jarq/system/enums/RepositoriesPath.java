754bfc6d11de1273cc2b4ec9328e3406ac4bf55b
jarqprog <jarqprog@gmail.com>
2018-05-03 19:02:23 +0200

    add new path to enum

--- src/main/java/com/jarq/system/enums/RepositoriesPath.java
+++ src/main/java/com/jarq/system/enums/RepositoriesPath.java
@@ -8,10 +8,14 @@ public enum RepositoriesPath {
     TEST_FILES_REPOSITORY("src/main/resources/testRepositories/border/"),
     READER_TEST_FILE("src/main/resources/testRepositories/readerTextFile.md"),
     WRITER_TEST_FILE("src/main/resources/testRepositories/writerTextFile.md"),
-    MANAGER_PATH_HAS_FILE_TEST("src/main/resources/testRepositories/border/repositoryManager/hasFile/1.md"),
-    MANAGER_PATH_CREATION_TEST("src/main/resources/testRepositories/border/repositoryManager/create/1/1/1.md"),
+    MANAGER_PATH_HAS_FILE_TEST("src/main/resources/testRepositories/border/" +
+            "repositoryManager/hasFile/1.md"),
+    MANAGER_PATH_CREATION_TEST("src/main/resources/testRepositories/border/" +
+            "repositoryManager/create/1/1/1.md"),
     MANAGER_PATH_REMOVE_FILE_TEST("src/main/resources/testRepositories/border/" +
-            "repositoryManager/removeFile/1/1/1/2018_10_10_20_21_21.md");
+            "repositoryManager/removeFile/1/1/1/2018_10_10_20_21_21.md"),
+    MANAGER_PATH_REMOVE_TEXT_DIRECTORY_TEST("src/main/resources/testRepositories/border/" +
+            "repositoryManager/removeTextDirectory/1/1/1/");
 
     private String path;
 
