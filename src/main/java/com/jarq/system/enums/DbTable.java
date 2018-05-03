3ef071b19fbab8ca6eb0da039bccb64e1a2365c8
jarqprog <jarqprog@gmail.com>
2018-05-03 08:52:11 +0200

    add new contents table to enum

--- src/main/java/com/jarq/system/enums/DbTable.java
+++ src/main/java/com/jarq/system/enums/DbTable.java
@@ -2,7 +2,7 @@ package com.jarq.system.enums;
 
 public enum DbTable {
 
-    REPOSITORIES, USERS, TEXTS, ADDRESSES;
+    REPOSITORIES, USERS, TEXTS, ADDRESSES, CONTENTS;
 
     public String getTable() {
         return this.toString().toLowerCase();
