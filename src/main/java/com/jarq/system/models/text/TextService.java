bff166cf955b8ece54a6e9e93d2b3c2e3774411b
jarqprog <jarqprog@gmail.com>
2018-05-04 17:37:37 +0200

    implement contrustor with dependencies in TextService

--- src/main/java/com/jarq/system/models/text/TextService.java
+++ src/main/java/com/jarq/system/models/text/TextService.java
@@ -1,13 +1,39 @@
 package com.jarq.system.models.text;
 
+import com.jarq.system.managers.filesManagers.IContentReader;
+import com.jarq.system.managers.filesManagers.IContentWriter;
+import com.jarq.system.managers.filesManagers.IRepositoryManager;
+import com.jarq.system.models.content.IDaoContent;
+
 public class TextService implements ITextService {
 
+    private final IRepositoryManager repositoryManager;
+    private final IContentReader<String> contentReader;
+    private final IContentWriter<String> contentWriter;
+    private final IDaoText daoText;
+    private final IDaoContent daoContent;
 
-    public static ITextService getInstance() {
-            return new TextService();
-        }
 
-    private TextService() {}
+    public static ITextService getInstance(IRepositoryManager repositoryManager,
+                                           IContentReader<String> contentReader,
+                                           IContentWriter<String> contentWriter,
+                                           IDaoText daoText,
+                                           IDaoContent daoContent) {
+        return new TextService( repositoryManager, contentReader,
+                contentWriter, daoText, daoContent);
+    }
+
+    private TextService(IRepositoryManager repositoryManager,
+                       IContentReader<String> contentReader,
+                       IContentWriter<String> contentWriter,
+                       IDaoText daoText,
+                       IDaoContent daoContent) {
+        this.repositoryManager = repositoryManager;
+        this.contentReader = contentReader;
+        this.contentWriter = contentWriter;
+        this.daoText = daoText;
+        this.daoContent = daoContent;
+    }
 
     @Override
     public IText createText() {
