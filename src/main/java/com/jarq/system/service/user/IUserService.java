31afeddf3f1e21c0044e3d9007298f80b9f71971
jarqprog <jarqprog@gmail.com>
2018-05-04 20:27:36 +0200

    add dependencies to UserService

--- src/main/java/com/jarq/system/service/user/IUserService.java
+++ src/main/java/com/jarq/system/service/user/IUserService.java
@@ -1,11 +1,18 @@
 package com.jarq.system.service.user;
 
+import com.jarq.system.exceptions.ServiceException;
 import com.jarq.system.service.IService;
 
 public interface IUserService extends IService {
 
     String createUser(String name, String surname, String email);
 
-    boolean createNote(int userId, int repositoryId, String title, String content);
-    
+    String changeUserName(int userId, String name);
+
+    String changeUserSurname(int userId, String surname);
+
+    String changeUserEmail(int userId, String email) throws ServiceException;
+
+    String changeUserPassword(int userId, String password) throws ServiceException;
+
 }
--- src/main/java/com/jarq/system/service/user/UserService.java
+++ src/main/java/com/jarq/system/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.jarq.system.service.user;
 
 import com.jarq.system.exceptions.DaoFailure;
+import com.jarq.system.exceptions.ServiceException;
 import com.jarq.system.models.address.IDaoAddress;
 import com.jarq.system.models.repository.IDaoRepository;
+import com.jarq.system.policy.IEmailPolicy;
+import com.jarq.system.policy.IPasswordPolicy;
 import com.jarq.system.service.text.ITextService;
 import com.jarq.system.models.user.IDaoUser;
 import com.jarq.system.models.user.IUser;
@@ -13,20 +16,27 @@ public class UserService implements IUserService {
     private final IDaoAddress daoAddress;
     private final IDaoRepository daoRepository;
     private final ITextService textService;
+    private final IEmailPolicy emailPolicy;
+    private final IPasswordPolicy passwordPolicy;
 
 
 
     public static IUserService getInstance(IDaoUser daoUser, IDaoRepository daoRepository,
-                                           IDaoAddress daoAddress, ITextService textService) {
-        return new UserService(daoUser, daoRepository, daoAddress, textService);
+                                           IDaoAddress daoAddress, ITextService textService,
+                                           IEmailPolicy emailPolicy, IPasswordPolicy passwordPolicy) {
+        return new UserService(daoUser, daoRepository, daoAddress, textService,
+                emailPolicy, passwordPolicy);
     }
 
     private UserService(IDaoUser daoUser, IDaoRepository daoRepository,
-                        IDaoAddress daoAddress, ITextService textService) {
+                        IDaoAddress daoAddress, ITextService textService,
+                        IEmailPolicy emailPolicy, IPasswordPolicy passwordPolicy) {
         this.daoUser = daoUser;
         this.daoRepository = daoRepository;
         this.daoAddress = daoAddress;
         this.textService = textService;
+        this.emailPolicy = emailPolicy;
+        this.passwordPolicy = passwordPolicy;
     }
 
     @Override
@@ -44,11 +54,25 @@ public class UserService implements IUserService {
         }
     }
 
-
-
+    @Override
+    public String changeUserName(int userId, String name) {
+        return null;
+    }
 
     @Override
-    public boolean createNote(int userId, int repositoryId, String title, String content) {
-        return false;
+    public String changeUserSurname(int userId, String surname) {
+        return null;
     }
+
+    @Override
+    public String changeUserEmail(int userId, String email) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public String changeUserPassword(int userId, String password) throws ServiceException {
+        return null;
+    }
+
+
 }
