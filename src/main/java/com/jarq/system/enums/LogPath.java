d74d38bd55143b0bd530139f98afb18351586dd0
jarqprog <jarqprog@gmail.com>
2018-05-05 10:24:41 +0200

    add LogPath enum

---
+++ src/main/java/com/jarq/system/enums/LogPath.java
@@ -0,0 +1,18 @@
+package com.jarq.system.enums;
+
+import java.io.File;
+
+public enum LogPath {
+
+    LOGGER("src/main/resources/logs/log/");
+
+    private String path;
+
+    LogPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path.replaceAll("/", File.separator);  // platform independent
+    }
+}
