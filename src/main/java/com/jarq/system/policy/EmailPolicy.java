eb54194047400ff3cb9b3d4aa4ac2e8c218a2a8d
jarqprog <jarqprog@gmail.com>
2018-05-06 13:58:13 +0200

    implement email policy with test

--- src/main/java/com/jarq/system/policy/EmailPolicy.java
+++ src/main/java/com/jarq/system/policy/EmailPolicy.java
@@ -1,11 +1,23 @@
 package com.jarq.system.policy;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailPolicy implements IEmailPolicy {
 
+    /**
+     * source: https://stackoverflow.com/questions/8204680/java-regex-email
+     */
+
+    private final Pattern VALID_EMAIL_ADDRESS_REGEX;
+
+    public EmailPolicy() {
+        this.VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    }
 
     @Override
     public boolean validate(String email) {
-        return true;
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+        return matcher.find();
     }
-
 }
